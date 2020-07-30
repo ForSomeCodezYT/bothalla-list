03adadc3c9ebb9a4be3813a28a70c64a0a634395
Glitch (maple-respected-fin) <none>
2020-07-30 21:18:53 +0000

    🧀🌸 Checkpoint ./src/routes/login.js:18173628/346 ./src/public/add/index.html:18173628/149

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,7 +4,7 @@ const { CLIENT_ID, DOMAIN } = process.env;
 const route = Router();
 
 route.get("/", async (req, res, next) => {
-    res.redirect(`https://discord.com/api/oauth2/authorize?client_id=737680912388587521&redirect_uri=https%3A%2F%2Fbothalla-list.glitch.me%2Fapi%2Fcallback&response_type=code&scope=identify`);
+    res.redirect(`https://discord.com/oauth2/authorize?client_id=737680912388587521&redirect_uri=https%3A%2F%2Fdiscord.bothallalist.ga%2Fapi%2Fcallback&response_type=code&scope=identify`);
 });
 
 module.exports = route;
